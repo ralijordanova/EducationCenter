7f60b05edb545a34c485c9c741c39975568fbcfe
Ralica1234 <rali_v666@abv.bg>
2021-08-01 17:38:13 +0300

    Part of model binding plus favicon

---
+++ EducCenter/Controllers/TeachersController.cs
@@ -0,0 +1,19 @@
+﻿
+namespace EducCenter.Controllers
+{
+    using EducCenter.Models.Teachers;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TeachersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(AddTeacherFormModel teacher)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EducCenter/Models/Teachers/AddTeacherFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducCenter.Models.Teachers
+{
+    public class AddTeacherFormModel
+    {
+        //kakvo populva usera za 1 teacher - TOVA E MODEL BINDING
+        public string Name { get; init; }
+        public string PhoneNumber { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+        //public int ChildId { get; init; }
+
+        //public ICollection<TeacherCourse> Courses
+    }
+}
