6181ce3a4910df59790cb5520bf4ce9020cf57e6
Ralica1234 <rali_v666@abv.bg>
2021-10-16 17:32:37 +0300

    Adding statistic ih home page

---
+++ EducCenter/Models/Teachers/AllTeachersViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace EducCenter.Models.Teachers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class AllTeachersViewModel
+    {
+
+    }
+}
---
+++ EducCenter/Models/Teachers/TeacherListingViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace EducCenter.Models.Teachers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class TeacherListingViewModel
+    {
+        public int Id { get; init; }     
+        public string Name { get; init; }
+        public string PhoneNumber { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+
+    }
+}
